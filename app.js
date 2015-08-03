601b921ce6ebcd3ecd0bb84592aa62603c98636c
oroce <robert@oroszi.net>
2015-08-03 17:14:52 +0200

    new jobs

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var http = require('http');
 var path = require('path');
 
 var app = express();
-
+app.locals.pretty = true;
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
